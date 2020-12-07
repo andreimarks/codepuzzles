139f050e36d2bb4e061434f86840df76b92adca3
Andrei <hey@ndrei.us>
2020-12-06 21:13:57 -0800

    Late add of Advent of Code Day 4 Part 2.

--- adventofcode/2020/aoc_2020_04.py
+++ adventofcode/2020/aoc_2020_04.py
@@ -45,21 +45,29 @@ According to the above rules, your improved system would report 2 valid passport
 Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?"""
 
 class Passport:
-    required_fields = ["byr",
-                       "iyr",
-                       "eyr",
-                       "hgt",
-                       "hcl",
-                       "ecl",
-                       "pid"]
+    required_fields = {
+        "byr": (lambda x: int(x) in range(1920, 2002 + 1)),
+        "iyr": (lambda x: int(x) in range(2010, 2020 + 1)),
+        "eyr": (lambda x: int(x) in range(2020, 2030 + 1)),
+        "hgt": (lambda x: int(x[:-2]) in range(150, 193 + 1) if x[-2:] == "cm" else (int(x[:-2]) in range(59, 76 + 1) if x[-2:] == "in" else False)),
+        "hcl": (lambda x: re.match("^\#[0-9a-f]{6}$", x)),
+        "ecl": (lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]),
+        "pid": (lambda x: re.match("^\d{9}$", x))}
 
     def __init__(self, fields):
         self.fields = fields
     
-    def is_valid(self):
+    def has_required_fields(self):
         match_count = len([field for field in self.required_fields if field in self.fields]) 
         req_count = len(self.required_fields)
         return match_count == req_count
+    
+    def is_valid(self):
+        for key, value in self.required_fields.items():
+            if not value(self.fields[key]):
+                return False
+
+        return True
         
 
 def get_passports(filename):
@@ -82,12 +90,78 @@ def get_passports(filename):
         # Append final passport.
         passports.append(Passport(passport_fields))
         
-
-
     return passports
 
+def solve_part_one(passports):
+    print(len([passport for passport in passports if passport.has_required_fields()]))
+
+"""--- Part Two ---
+The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+byr valid:   2002
+byr invalid: 2003
+
+hgt valid:   60in
+hgt valid:   190cm
+hgt invalid: 190in
+hgt invalid: 190
+
+hcl valid:   #123abc
+hcl invalid: #123abz
+hcl invalid: 123abc
+
+ecl valid:   brn
+ecl invalid: wat
+
+pid valid:   000000001
+pid invalid: 0123456789
+Here are some invalid passports:
+
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+Here are some valid passports:
+
+pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?"""
+
+def solve_part_two(passports):
+    print(len([passport for passport in passports if passport.has_required_fields() and passport.is_valid()]))
+
 passports = get_passports("aoc_2020_04_input")
-for passport in passports:
-    if not passport.is_valid():
-        print(str(len(passport.fields)) + ": " + str(passport.fields))
-print(len([passport for passport in passports if passport.is_valid()]))
+solve_part_two(passports)
