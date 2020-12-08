96570e272bf8b886d42269bd6399088084248075
Andrei <hey@ndrei.us>
2020-12-08 14:08:49 -0800

    Advent of Code Day 7 Part 2

--- adventofcode/2020/aoc_2020_07.py
+++ adventofcode/2020/aoc_2020_07.py
@@ -69,7 +69,7 @@ class Rule:
             match = re.match(pattern, bag)
             count = match.group(1)
             name = match.group(2)
-            self.contents[name] = count
+            self.contents[name] = int(count)
 
 
     def get_name(self):
@@ -96,13 +96,10 @@ def get_rules(input):
 
 
 def search_rules_for_contents(rules, items):
-    results = []
-    results.extend([rule.get_name() for rule in rules.values() if rule.contains_bag_type(*items)])
-    return results
+    return [rule.get_name() for rule in rules.values() if rule.contains_bag_type(*items)]
 
 
-def solve_part_one(input):
-    rules = get_rules(input)
+def solve_part_one(rules):
     results = search_rules_for_contents(rules, ["shiny gold"])
     results_set = set(results)
 
@@ -112,4 +109,45 @@ def solve_part_one(input):
 
     print(len(results_set))
 
+"""--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?"""
+
+def add_contents(rules, all_bags, origin):
+    for bag, count in origin.contents.items():
+        for i in range(count):
+            all_bags.append(bag)
+            add_contents(rules, all_bags, rules[bag])
+
+def solve_part_two(rules):
+    all_bags = []
+    origin = rules["shiny gold"]
+    add_contents(rules, all_bags, origin)
+
+    print(len(all_bags))
+
 input = get_list_from_file("aoc_2020_07_input")
+rules = get_rules(input)
+solve_part_two(rules)
