c62addaa3036e22fa990e5d3542d5a3a4b964561
Andrei <hey@ndrei.us>
2020-12-18 04:54:00 -0800

    Advent of Code 2020 Day 12 Part 2 in progress.

--- adventofcode/2020/aoc_2020_12.py
+++ adventofcode/2020/aoc_2020_12.py
@@ -40,12 +40,13 @@ class Ship:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.wp_x = 10
+        self.wp_y = 1
         self.direction = 90 
 
 
     def move(self, line):
-        command = line[0]
-        value = int(line[1:])
+        command, value = parse_line(line)
 
         if command == "R":
             self.direction += value
@@ -62,7 +63,11 @@ class Ship:
             self.x += value
         elif command == "W":
             self.x -= value
+    
+    def parse_line(self, line):
+        return line[0], int(line[1:])
 
+    def process_line(self, line):)
 
     def get_direction_from_degrees(self, value):
         index = round(value / (360. / len(Ship.dirs)))
@@ -74,6 +79,30 @@ class Ship:
     def get_manhattan_distance(self):
         return abs(self.x) + abs(self.y)
         
+"""--- Part Two ---
+Before you can give the destination to the captain, you realize that the actual action meanings were printed on the back of the instructions the whole time.
+
+Almost all of the actions indicate how to move a waypoint which is relative to the ship's position:
+
+Action N means to move the waypoint north by the given value.
+Action S means to move the waypoint south by the given value.
+Action E means to move the waypoint east by the given value.
+Action W means to move the waypoint west by the given value.
+Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+Action F means to move forward to the waypoint a number of times equal to the given value.
+The waypoint starts 10 units east and 1 unit north relative to the ship. The waypoint is relative to the ship; that is, if the ship moves, the waypoint moves with it.
+
+For example, using the same instructions as above:
+
+F10 moves the ship to the waypoint 10 times (a total of 100 units east and 10 units north), leaving the ship at east 100, north 10. The waypoint stays 10 units east and 1 unit north of the ship.
+N3 moves the waypoint 3 units north to 10 units east and 4 units north of the ship. The ship remains at east 100, north 10.
+F7 moves the ship to the waypoint 7 times (a total of 70 units east and 28 units north), leaving the ship at east 170, north 38. The waypoint stays 10 units east and 4 units north of the ship.
+R90 rotates the waypoint around the ship clockwise 90 degrees, moving it to 4 units east and 10 units south of the ship. The ship remains at east 170, north 38.
+F11 moves the ship to the waypoint 11 times (a total of 44 units east and 110 units south), leaving the ship at east 214, south 72. The waypoint stays 4 units east and 10 units south of the ship.
+After these operations, the ship's Manhattan distance from its starting position is 214 + 72 = 286.
+
+Figure out where the navigation instructions actually lead. What is the Manhattan distance between that location and the ship's starting position?"""
 
 test_input = """F10
 N3
