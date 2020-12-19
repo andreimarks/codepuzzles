949aa3ebf395bcf01c66e12b168823d1bc3bcc9f
Andrei <hey@ndrei.us>
2020-12-19 10:03:27 -0800

    Advent of Code 2020 Day 13 Part 1

---
+++ adventofcode/2020/aoc_2020_13.py
@@ -0,0 +1,80 @@
+from helpers import get_list_from_file
+
+"""--- Day 13: Shuttle Search ---
+Your ferry can make it safely to a nearby port, but it won't get much further. When you call to book another ship, you discover that no ships embark from that port to your vacation island. You'll need to get from the port to the nearest airport.
+
+Fortunately, a shuttle bus service is available to bring you from the sea port to the airport! Each bus has an ID number that also indicates how often the bus leaves for the airport.
+
+Bus schedules are defined based on a timestamp that measures the number of minutes since some fixed reference point in the past. At timestamp 0, every bus simultaneously departed from the sea port. After that, each bus travels to the airport, then various other locations, and finally returns to the sea port to repeat its journey forever.
+
+The time this loop takes a particular bus is also its ID number: the bus with ID 5 departs from the sea port at timestamps 0, 5, 10, 15, and so on. The bus with ID 11 departs at 0, 11, 22, 33, and so on. If you are there when the bus departs, you can ride that bus to the airport!
+
+Your notes (your puzzle input) consist of two lines. The first line is your estimate of the earliest timestamp you could depart on a bus. The second line lists the bus IDs that are in service according to the shuttle company; entries that show x must be out of service, so you decide to ignore them.
+
+To save time once you arrive, your goal is to figure out the earliest bus you can take to the airport. (There will be exactly one such bus.)
+
+For example, suppose you have the following notes:
+
+939
+7,13,x,x,59,x,31,19
+Here, the earliest timestamp you could depart is 939, and the bus IDs in service are 7, 13, 59, 31, and 19. Near timestamp 939, these bus IDs depart at the times marked D:
+
+time   bus 7   bus 13  bus 59  bus 31  bus 19
+929      .       .       .       .       .
+930      .       .       .       D       .
+931      D       .       .       .       D
+932      .       .       .       .       .
+933      .       .       .       .       .
+934      .       .       .       .       .
+935      .       .       .       .       .
+936      .       D       .       .       .
+937      .       .       .       .       .
+938      D       .       .       .       .
+939      .       .       .       .       .
+940      .       .       .       .       .
+941      .       .       .       .       .
+942      .       .       .       .       .
+943      .       .       .       .       .
+944      .       .       D       .       .
+945      D       .       .       .       .
+946      .       .       .       .       .
+947      .       .       .       .       .
+948      .       .       .       .       .
+949      .       D       .       .       .
+The earliest bus you could take is bus ID 59. It doesn't depart until timestamp 944, so you would need to wait 944 - 939 = 5 minutes before it departs. Multiplying the bus ID by the number of minutes you'd need to wait gives 295.
+
+What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus?"""
+
+test_input = """939
+7,13,x,x,59,x,31,19""".split("\n")
+
+input = get_list_from_file("aoc_2020_13_input")
+
+input_to_use = input #test_input
+
+arrival_time = timestamp = int(input_to_use[0])
+schedule = input_to_use[1].split(",")
+buses = [int(bus) for bus in schedule if bus != "x"]
+
+"""
+target_bus = None
+while target_bus is None:
+    for b in buses:
+        if (timestamp % b == 0):
+            target_bus = b
+    timestamp += 1
+
+wait = timestamp - 1 - arrival_time
+solution = target_bus * wait
+print(solution)
+"""
+i = 0
+while i < 3500:
+    base = 17 * i
+    if (base + 2) % 13 == 0 and (base + 3) % 19 == 0:
+        break
+    i += 1
+print(base)
+print(i % 17)
+print((i+2) % 13)
+print((i+3) % 19)
