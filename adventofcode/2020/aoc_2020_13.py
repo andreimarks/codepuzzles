4a9507f4a0a9b7fad92009590324be9e2b250d3a
Andrei <hey@ndrei.us>
2020-12-20 16:17:17 -0800

    Advent of Code Day 13 Part 2 in progress.

--- adventofcode/2020/aoc_2020_13.py
+++ adventofcode/2020/aoc_2020_13.py
@@ -45,36 +45,142 @@ The earliest bus you could take is bus ID 59. It doesn't depart until timestamp 
 
 What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus?"""
 
+def solve_part_one(input):
+    arrival_time = timestamp = int(input_to_use[0])
+    schedule = input_to_use[1].split(",")
+    buses = [int(bus) for bus in schedule if bus != "x"]
+
+    target_bus = None
+    while target_bus is None:
+        for b in buses:
+            if (timestamp % b == 0):
+                target_bus = b
+        timestamp += 1
+
+    wait = timestamp - 1 - arrival_time
+    solution = target_bus * wait
+    print(solution) #5257
+
+"""--- Part Two ---
+The shuttle company is running a contest: one gold coin for anyone that can find the earliest timestamp such that the first bus ID departs at that time and each subsequent listed bus ID departs at that subsequent minute. (The first line in your input is no longer relevant.)
+
+For example, suppose you have the same list of bus IDs as above:
+
+7,13,x,x,59,x,31,19
+An x in the schedule means there are no constraints on what bus IDs must depart at that time.
+
+This means you are looking for the earliest timestamp (called t) such that:
+
+Bus ID 7 departs at timestamp t.
+Bus ID 13 departs one minute after timestamp t.
+There are no requirements or restrictions on departures at two or three minutes after timestamp t.
+Bus ID 59 departs four minutes after timestamp t.
+There are no requirements or restrictions on departures at five minutes after timestamp t.
+Bus ID 31 departs six minutes after timestamp t.
+Bus ID 19 departs seven minutes after timestamp t.
+The only bus departures that matter are the listed bus IDs at their specific offsets from t. Those bus IDs can depart at other times, and other bus IDs can depart at those times. For example, in the list above, because bus ID 19 must depart seven minutes after the timestamp at which bus ID 7 departs, bus ID 7 will always also be departing with bus ID 19 at seven minutes after timestamp t.
+
+In this example, the earliest timestamp at which this occurs is 1068781:
+
+time     bus 7   bus 13  bus 59  bus 31  bus 19
+1068773    .       .       .       .       .
+1068774    D       .       .       .       .
+1068775    .       .       .       .       .
+1068776    .       .       .       .       .
+1068777    .       .       .       .       .
+1068778    .       .       .       .       .
+1068779    .       .       .       .       .
+1068780    .       .       .       .       .
+1068781    D       .       .       .       .
+1068782    .       D       .       .       .
+1068783    .       .       .       .       .
+1068784    .       .       .       .       .
+1068785    .       .       D       .       .
+1068786    .       .       .       .       .
+1068787    .       .       .       D       .
+1068788    D       .       .       .       D
+1068789    .       .       .       .       .
+1068790    .       .       .       .       .
+1068791    .       .       .       .       .
+1068792    .       .       .       .       .
+1068793    .       .       .       .       .
+1068794    .       .       .       .       .
+1068795    D       D       .       .       .
+1068796    .       .       .       .       .
+1068797    .       .       .       .       .
+In the above example, bus ID 7 departs at timestamp 1068788 (seven minutes after t). This is fine; the only requirement on that minute is that bus ID 19 departs then, and it does.
+
+Here are some other examples:
+
+The earliest timestamp that matches the list 17,x,13,19 is 3417.
+67,7,59,61 first occurs at timestamp 754018.
+67,x,7,59,61 first occurs at timestamp 779210.
+67,7,x,59,61 first occurs at timestamp 1261476.
+1789,37,47,1889 first occurs at timestamp 1202161486.
+However, with so many bus IDs in your list, surely the actual earliest timestamp will be larger than 100000000000000!
+
+What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list?"""
+
+def test_bus_schedule_modulus(schedule):
+    buses = [(index, int(number)) for index, number in enumerate(schedule) if number != "x"]
+    buses.sort(key = lambda x: x[1], reverse = True)
+    print(buses)
+
+    target_timestamp = None
+    base_index = buses[0][0]
+    base_number = buses[0][1]
+    multiplier = 0
+
+    #print(base_index)
+    #print(base_number)
+    while target_timestamp is None:
+        multiplier += 1
+        base_timestamp = base_number * multiplier
+        #print(base_timestamp)
+
+        reached_end = True
+        for bus in buses[1:]:
+            #print(bus)
+            index = bus[0]
+            modulus = bus[1]
+
+            time_diff = bus[0] - base_index
+            #print(time_diff)
+            eval_timestamp = base_timestamp + time_diff
+            #print(eval_timestamp)
+
+            #print(eval_timestamp % modulus)
+            if eval_timestamp % modulus == 0:
+                continue
+            else: 
+                reached_end = False
+                break
+
+        if reached_end:
+            target_timestamp = base_timestamp
+
+    print(target_timestamp - base_index)
+
+def test_test_schedules(test_schedules):
+    schedules = [schedule.split(",") for schedule in test_schedules]
+    print([test_bus_schedule(schedule) for schedule in schedules])
+
 test_input = """939
 7,13,x,x,59,x,31,19""".split("\n")
 
+test_schedules = """17,x,13,19
+67,7,59,61
+67,x,7,59,61
+67,7,x,59,61
+1789,37,47,1889""".split("\n")
+
 input = get_list_from_file("aoc_2020_13_input")
 
-input_to_use = input #test_input
+#input_to_use = test_input
+input_to_use = input
 
-arrival_time = timestamp = int(input_to_use[0])
+#solve_part_one(input_to_use)
+#test_test_schedules(test_schedules)
+
 schedule = input_to_use[1].split(",")
-buses = [int(bus) for bus in schedule if bus != "x"]
-
-"""
-target_bus = None
-while target_bus is None:
-    for b in buses:
-        if (timestamp % b == 0):
-            target_bus = b
-    timestamp += 1
-
-wait = timestamp - 1 - arrival_time
-solution = target_bus * wait
-print(solution)
-"""
-i = 0
-while i < 3500:
-    base = 17 * i
-    if (base + 2) % 13 == 0 and (base + 3) % 19 == 0:
-        break
-    i += 1
-print(base)
-print(i % 17)
-print((i+2) % 13)
-print((i+3) % 19)
+test_bus_schedule(schedule)
